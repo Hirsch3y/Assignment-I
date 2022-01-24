23d0926481d8f08c22ff747ddf3f17d2b5c7cb41
Hirsch3y <97813504+Hirsch3y@users.noreply.github.com>
2022-01-23 20:44:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-      require(["esri/Map", "esri/views/MapView"], (Map, MapView) => {
-        const map = new Map({
-          basemap: "topo-vector"
-        });
+var map = L.map('mapid').setView([38.626835,-90.21239], 18);
 
-        const view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 13,
-          center: [-90.2, 38.6] // longitude, latitude
-        });
-       
-var theGrove=L.marker([-90.25434454458241,38.627400228141404]).addTo(map).bindPopup('Sultan Mediterranean Restaurant').openOn(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 11,
+  minZoom:8,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var theGrove=L.marker([38.627400228141404,-90.25434454458241]).addTo(map).bindPopup('Sultan Mediterranean Restaurant');
         
-var   centralwestend=L.marker([-90.26080783227775,38.65082907398419]).addTo(map).bindPopup('Ranoush').openOn(map);
+var centralwestend=L.marker([38.65082907398419,-90.26080783227775]).addTo(map).bindPopup('Ranoush');
 
-var southcounty=L.marker([-90.37811280225854,38.50434951523715]).addTo(map).bindPopup('Pad Thai').openOn(map);
-    
-var soulard=L.marker([-90.2054408157469,38.60893358482006]).addTo(map).bindPopup('Chava’s Mexican Restaurant').openOn(map);
+var southcounty=L.marker([38.50434951523715,-90.37811280225854]).addTo(map).bindPopup('Pad Thai');
+ 
+var soulard=L.marker([38.60893358482006,-90.2054408157469]).addTo(map).bindPopup('Chava’s Mexican Restaurant');
 
-var thehill=L.marker([-90.2686562482886,38.61667495683198]).addTo(map).bindPopup('Rigazzi’s Italian Restaurant').openOn(map);
-      });
+var thehill=L.marker([38.61667495683198,-90.2686562482886]).addTo(map).bindPopup('Rigazzi’s Italian Restaurant');
